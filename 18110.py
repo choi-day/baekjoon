fc1fa8bf8542e1839fc64e414bfe554225c9a0aa
choi-day <choisschoco@gmail.com>
2023-06-25 21:36:32 +0900

    round error

--- 18110.py
+++ 18110.py
@@ -3,19 +3,22 @@ from collections import deque
 
 n = int(input())
 
+def round2(num):
+    return int(num) + (1 if num - int(num) >= 0.5 else 0)
+
 if(n == 0): print(0)
+else:
+    li = []
 
-li = []
+    for _ in range(n):
+        i = int(sys.stdin.readline().strip())
+        li.append(i)
 
-for _ in range(n):
-    i = int(sys.stdin.readline().strip())
-    li.append(i)
+    d = deque(sorted(li))
+    count = round2(n * 0.15)
 
-d = deque(sorted(li))
-count = round(n * 0.3)
+    for _ in range(count):
+        d.popleft()
+        d.pop()
 
-for _ in range(count):
-    d.popleft()
-    d.pop()
-
-print(round(sum(d)/len(d)))
+    print(round2(sum(d)/len(d)))
