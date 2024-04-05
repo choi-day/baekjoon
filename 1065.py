b0bc4c9a94d3f2628d3853296a262a0dff693a9c
choi-day <choisschoco@gmail.com>
2024-04-05 21:04:35 +0900

    20240405

---
+++ 1065.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+count = 0
+
+for n in range(1, n+1) :
+    if n <= 99 : 
+        count += 1 
+    
+    else :     
+        nums = list(map(int, str(n)))
+        if nums[0] - nums[1] == nums[1] - nums[2] :
+            count+=1
